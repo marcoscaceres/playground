25f74acbf1d14ee1fdfeb30660ce7d899e3330e1
Marcos Cáceres <marcos@marcosc.com>
2018-02-22 13:39:44 -0800

    dsafas

--- payrequest.js
+++ payrequest.js
@@ -8,62 +8,71 @@ const methodData = [
 ];
 
 const details = {
-  displayItems: [{
-    label: "test",
-    amount: { currency: "USD", value: "1000.00" },
-  }],
   total: {
     label: "Total due",
     amount: { currency: "USD", value: "1000.00" },
   }
 }
 
-// const creditCardFee = {
-//   label: "Credit card processing fee",
-//   amount: { currency: "USD", value: "3.00" },
-// };
+const creditCardFee = {
+  label: "Credit card processing fee",
+  amount: { currency: "USD", value: "3.00" },
+};
 
-// const debitCardFee = {
-//   label: "Debit card processing fee",
-//   amount: { currency: "USD", value: "6.00" },
-// };
+const debitCardFee = {
+  label: "Debit card processing fee",
+  amount: { currency: "USD", value: "6.00" },
+};
 
-// // Modifiers apply when the user chooses to pay with
-// // a credit card.
-// const modifiers = [
-//   {
-//     additionalDisplayItems: [creditCardFee],
-//     supportedMethods: ["basic-card"],
-//     total: {
-//       label: "Total due",
-//       amount: { currency: "USD", value: "500.00" },
-//     },
-//     data: {
-//       supportedTypes: "credit",
-//     },
-//   },
-//   {
-//     additionalDisplayItems: [debitCardFee],
-//     supportedMethods: ["basic-card"],
-//     total: {
-//       label: "Total due",
-//       amount: { currency: "USD", value: "2000.00" },
-//     },
-//     data: {
-//       supportedTypes: "debit",
-//     },
-//   }
-// ];
-// Object.assign(details, { modifiers });
+// Modifiers apply when the user chooses to pay with
+// a credit card.
+const modifiers = [
+  {
+    additionalDisplayItems: [creditCardFee],
+    supportedMethods: "basic-card",
+    total: {
+      label: "Total due",
+      amount: { currency: "USD", value: "500.00" },
+    },
+    data: {
+      supportedTypes: "credit",
+    },
+  },
+  {
+    additionalDisplayItems: [debitCardFee],
+    supportedMethods: "basic-card",
+    total: {
+      label: "Total due",
+      amount: { currency: "USD", value: "2000.00" },
+    },
+    data: {
+      supportedTypes: "debit",
+    },
+  }
+];
+Object.assign(details, { modifiers });
 
 async function doPaymentRequest() {
   try {
-    console.log("OH HAI!!!");
-    const request = new PaymentRequest(methodData, details, {requestPayerName: true});
+    const request = new PaymentRequest(methodData, details);
     // See below for a detailed example of handling these events
     const response = await request.show();
+    await validateResponse(response);
   } catch (err) {
     // AbortError, SecurityError
     console.error(err);
   }
 }
+async function validateResponse(response) {
+  try {
+    if (await checkAllValuesAreGood(response)) {
+      await response.complete("success");
+    } else {
+      await response.complete("fail");
+    }
+  } catch (err) {
+    // Something went wrong...
+    await response.complete("fail");
+  }
+}
+doPaymentRequest();
