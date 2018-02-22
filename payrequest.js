1fedc02e1e32f2cc82a4c52b92c216682abd77f1
Marcos Cáceres <marcos@marcosc.com>
2018-02-22 13:57:31 -0800

    dsafas

--- payrequest.js
+++ payrequest.js
@@ -1,6 +1,6 @@
 const methodData = [
   {
-    supportedMethods: ["basic-card"],
+    supportedMethods: ["visa"],
   },
 ];
 
@@ -10,44 +10,6 @@ const details = {
     amount: { currency: "USD", value: "1000.00" },
   }
 }
-
-// const creditCardFee = {
-//   label: "Credit card processing fee",
-//   amount: { currency: "USD", value: "3.00" },
-// };
-
-// const debitCardFee = {
-//   label: "Debit card processing fee",
-//   amount: { currency: "USD", value: "6.00" },
-// };
-
-// // Modifiers apply when the user chooses to pay with
-// // a credit card.
-// const modifiers = [
-//   {
-//     additionalDisplayItems: [creditCardFee],
-//     supportedMethods: ["basic-card"],
-//     total: {
-//       label: "Total due",
-//       amount: { currency: "USD", value: "500.00" },
-//     },
-//     data: {
-//       supportedTypes: ["credit"],
-//     },
-//   },
-//   {
-//     additionalDisplayItems: [debitCardFee],
-//     supportedMethods: ["basic-card"],
-//     total: {
-//       label: "Total due",
-//       amount: { currency: "USD", value: "2000.00" },
-//     },
-//     data: {
-//       supportedTypes: ["debit"],
-//     },
-//   }
-// ];
-// Object.assign(details, { modifiers });
 
 async function doPaymentRequest() {
   try {
