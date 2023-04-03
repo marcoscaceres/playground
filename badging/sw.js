4a4f8d2bc8eba3173352443c588f69d0e9d0182c
Marcos Caceres <marcos@marcosc.com>
2023-04-03 10:53:37 +1000

    badging update

--- badging/sw.js
+++ badging/sw.js
@@ -1,18 +1,30 @@
 // let's cache the app shell
-self.addEventListener('install', async function(event) {
-  event.waitUntil(
-    caches.open('my-cache').then(function(cache) {
-      return cache.addAll([
-        'index.html',
-      ]);
-    })
-  );
+self.addEventListener("install", async function (event) {
+  const cache = await caches.open("my-cache");
+  await cache.addAll(["index.html", "offline"]);
 });
+
 // make page work offline
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request).then(function(response) {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener("fetch", async function (event) {
+  const cachedResponse = await caches.match(event.request);
+  if (cachedResponse) {
+    return cachedResponse;
+  }
+
+  try {
+    const networkResponse = await fetch(event.request);
+    if (networkResponse.status === 200) {
+      const cache = await caches.open("my-cache");
+      await cache.put(event.request, networkResponse.clone());
+    }
+    return networkResponse;
+  } catch (error) {
+    // If the fetch failed, return an offline page or an error message
+    // depending on your app's requirements
+    const cachedOfflineResponse = await caches.match("offline.html");
+    if (cachedOfflineResponse) {
+      return cachedOfflineResponse;
+    }
+    throw error;
+  }
 });
