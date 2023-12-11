d7e20bdfc064b8e79d98f79b7be087e3dd88c2e2
Marcos Caceres <marcos@marcosc.com>
2023-12-11 15:50:29 +1100

    dual push

---
+++ push/serviceworker.js
@@ -0,0 +1,8 @@
+// serviceworker.js
+self.addEventListener('install', event => {
+    // Perform install steps, if any
+});
+
+self.addEventListener('push', event => {
+    // Handle push event
+});
